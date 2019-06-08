5e23e9135d1882b9e69f5a0e26a1b6b79445dfcd
ArturoBalleros <karveg7@gmail.com>
2019-06-08 18:50:39 +0200

    No encontrar el paquete no asignado

--- readyreq/paq_frag_list.php
+++ readyreq/paq_frag_list.php
@@ -19,7 +19,7 @@ else {
 		echo makeError("No2");
 	else {
 
-		$consulta = "Select Id,Nombre from Paquetes where Id <> 0 Order By Categoria Desc, Nombre;";
+		$consulta = "Select Id,Nombre from Paquetes where Id <> 1 Order By Categoria Desc, Nombre;";
 		$resultado = mysqli_query($conexion, $consulta);
 
 		if (!$resultado)
